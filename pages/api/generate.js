83b0b8f674675a89fca3f065ab11ed1b61b02598
kristinameyers <meyers.kristina@gmail..com>
2025-08-18 22:47:03 -0700

    update generate js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,8 +7,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+ if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
   }
 
   if (req.method !== 'POST') {
